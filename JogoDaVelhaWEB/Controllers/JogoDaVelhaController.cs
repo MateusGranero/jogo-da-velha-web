a0a4412f2bae7da2fb62cfab891db642688d319a
Mateus Granero <mateus@engsolutions.com.br>
2016-05-04 16:45:45 -0300

    Enviando Projeto

---
+++ JogoDaVelhaWEB/Controllers/JogoDaVelhaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using JogoDaVelhaWEB.Models;
+
+namespace JogoDaVelhaWEB.Controllers
+{
+    public class JogoDaVelhaController : Controller
+    {
+        // GET: JogoDaVelha
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult IniciarJogo(Jogo jogo)//string player1, string player2
+        {
+            return PartialView("_Jogo",jogo);
+        }
+    }
+}
---
+++ JogoDaVelhaWEB/Models/Jogo.cs
@@ -0,0 +1,8 @@
+﻿namespace JogoDaVelhaWEB.Models
+{
+    public class Jogo
+    {
+        public string Player1 { get; set; } 
+        public string Player2 { get; set; }
+    }
+}
